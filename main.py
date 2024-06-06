2fcbb6ca664894b89d193d7b84072e42c7953a0f
Yashwanth kumar <155567775+Yashwanthkumar999@users.noreply.github.com>
2024-06-06 13:24:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ articles = response2.json()["articles"]
 
 
   ## STEP 2: https://newsapi.org/
-  # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+  #  get the first 3 news pieces for the COMPANY_NAME.
 
 #TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 
@@ -97,18 +97,6 @@ if abs(diff_percent) > 1:
             from_=TWILIO_PHONE_NO,
             to="+917075912537"
         )
-    #TODO 9. - Send each article as a separate message via Twilio.
+    
 
-
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
